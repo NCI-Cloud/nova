aed4747f40c74b5666489995aa6d73417c479a65
Simon Fowler <simon.fowler@anu.edu.au>
2015-01-20 00:52:02 +0000

    One line commit to allow sphinx to run.
    It was trying to import oslosphinx rather than oslo.sphinx.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,7 +34,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.pngmath',
               'sphinx.ext.ifconfig',
               'sphinx.ext.graphviz',
-              'oslosphinx',
+              'oslo.sphinx',
               ]
 
 todo_include_todos = True
